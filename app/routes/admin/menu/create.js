525974341e067bc87369a38ca466fd23640b6cbc
Corbin Otwell <cbotwell@gmail.com>
2015-08-18 14:39:14 -0500

    adds model for menu-item

--- app/routes/admin/menu/create.js
+++ app/routes/admin/menu/create.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function() {
+    return this.store.createRecord('menu-item');
+  },
+  actions: {
+    save: function(menuItem) {
+      menuItem.save().then(() => {
+        this.transitionTo('admin.menu');
+      });
+    }
+  }
 });
